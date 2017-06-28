6c0ddd84bd83c249443e414e877056ef6809675c
Jean Yang <bdlas321@gmail.com>
2017-06-28 17:39:05 +0800

    fix styled component

--- src/Mail.js
+++ src/Mail.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import styled from 'styled-components';
-//
-// const MailContainer = styled`
-//   width: 100%;
-// `;
+import styled from 'styled-components';
+
+const MailContainer = styled.div`
+  width: 100%;
+`;
 
 function Mail() {
   return (
-    // <MailContainer>
+    <MailContainer>
       <h1>Title</h1>
-    // </MailContainer>
+    </MailContainer>
   );
 }
 
