11f98adb15f1c9e54775bda9fa064f45f6e77b59
Jean Yang <bdlas321@gmail.com>
2017-06-29 14:49:52 +0800

    install autoprefixer

--- config/postcss.js
+++ config/postcss.js
@@ -1,10 +1,12 @@
 const values = require('postcss-modules-values');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   loader: 'postcss-loader',
   options: {
     plugins: [
       values,
+      autoprefixer,
     ],
   },
 };
