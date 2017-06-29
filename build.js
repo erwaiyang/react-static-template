ebb7068d8f5382978d8f334849c8ea694e54f08f
Jean Yang <bdlas321@gmail.com>
2017-06-29 11:13:07 +0800

    remove unused css & minify

--- build.js
+++ build.js
@@ -1,10 +1,15 @@
 const childProcess = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const purifycss = require('purify-css');
 
 const body = childProcess.execSync(`node ${path.resolve(__dirname, './dist/app-bundle')}`);
 
 const html = fs.readFileSync('./index.html', 'utf8');
-const result = html.replace(/<body>([\s\S]*)<\/body>/gm, `<body>${body}</body>`);
-fs.writeFileSync('./dist/index.html', result);
+const css = fs.readFileSync('./dist/styles.css', 'utf8');
+const reactParsedHtml = html.replace(/<body>([\s\S]*)<\/body>/gm, `<body>${body}</body>`);
+
+purifycss(reactParsedHtml, css, { output: './dist/purified.css', minify: true });
+
+fs.writeFileSync('./dist/index.html', reactParsedHtml);
 console.log('Template Saved!');
