73510a92a1510622f8f9ccfd5c6792f8277f31f8
Jean Yang <bdlas321@gmail.com>
2017-06-29 11:03:11 +0800

    use sync version methods

--- build.js
+++ build.js
@@ -4,11 +4,7 @@ const fs = require('fs');
 
 const body = childProcess.execSync(`node ${path.resolve(__dirname, './dist/app-bundle')}`);
 
-fs.readFile('./index.html', 'utf8', (err, html) => {
-  if (err) throw err;
-  const result = html.replace(/<body>([\s\S]*)<\/body>/gm, `<body>${body}</body>`);
-  fs.writeFile('./dist/index.html', result, (werr) => {
-    if (werr) throw werr;
-    console.log('Template Saved!');
-  });
-});
+const html = fs.readFileSync('./index.html', 'utf8');
+const result = html.replace(/<body>([\s\S]*)<\/body>/gm, `<body>${body}</body>`);
+fs.writeFileSync('./dist/index.html', result);
+console.log('Template Saved!');
